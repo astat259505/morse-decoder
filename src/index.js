38a6361caaad0243beed8ebc156a1d03f25b3ade
Maxim <astat259505@gmail.com>
2022-02-22 22:25:03 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -37,10 +37,57 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+let expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
+
 function decode(expr) {
-    // write your solution here
+   let arr = []
+   let arr2 = []
+   for (i = 0; i < expr.length; i += 2) {
+      arr.push(expr.slice(i, (i + 2)))
+   }
+   
+  let arr3 = arr.map(item => {
+      if (item == '00') {
+         return item = ''
+      } if (item == '10') {
+         return item = '.'
+      } if (item == '11') {
+         return item = '-'
+      } if (item == '**') {
+         return item = ' '
+      }
+   })
+ 
+   for (i = 0; i < arr3.length; i += 5) {
+      arr2.push(arr3.slice(i, (i + 5)).join(''))
+   }
+   let arr4 = arr2.map(item => {
+      for (let key of Object.keys(MORSE_TABLE)) {
+         if (item == key) {
+            return item = MORSE_TABLE[key]
+         } if (item.includes(' ')) {
+            return item = ' '
+         }
+     }
+   
+   })
+
+
+   console.log(arr)
+   
+   console.log(arr3)
+   console.log(arr2)
+   console.log(arr4)
+   
+return arr4.join('')
+  
 }
 
+decode(expr)
+
+
+
 module.exports = {
-    decode
+    decode(expr)
 }
+
